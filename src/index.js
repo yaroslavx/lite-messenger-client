7e37d335c382f6d76ce06308f73f646f54e35495
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-10-17 17:24:54 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import store from './redux/store';
 import App from './App';
 import './index.css';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from 'react-router-dom';
-import Chat from './components/chat/Chat';
-import NameBlock from './components/nameBlock/NameBlock.jsx';
-import RoomBlock from './components/roomBlock/RoomBlock.jsx';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
